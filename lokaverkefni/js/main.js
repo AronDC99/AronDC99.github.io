90fdc03e889b8fe2738266aa9b10e8cd52d779c2
Aron DC <aron@merkiverk.net>
2018-05-15 22:26:02 +0000

    Add files via upload

--- lokaverkefni/js/main.js
+++ lokaverkefni/js/main.js
@@ -1,5 +1,6 @@
+$( document ).ready(function() {
 let concertsJson = $.ajax({
-  'url': 'http://apis.is/concerts',
+  'url': 'https://apis.is/concerts',
   'type': 'GET',
   'dataType': 'json',
   'success': function(response) {
@@ -9,20 +10,24 @@ let concertsJson = $.ajax({
 
 let mainFeatures = document.getElementById("mainFeatures");
 
-let concerts = JSON.parse(concertsJson);
+let htmlCode = '';
 
-let htmlCode = "";
-
-for(let key in concerts.results){
     
-    htmlCode += '<li class="item"><a href="/tonleikar/" class="feature" itemscope itemtype="http://schema.org/MusicEvent"><div class="artwork"><div id="1_10185" class="zoom" style="background-image:url(';
-    htmlCode += concerts.results[key].imageSource;
-    htmlCode += ');"></div></div><div class="caption"><div class="type music">T&#243;nleikar</div><div class="title"><h2 itemprop="name">';
-    htmlCode += concerts.results[key].eventDateName;
-    htmlCode += '</h2></div><div class="venue" itemprop="location">';
-    htmlCode += concerts.results[key].eventHallName;
-    htmlCode += '</div></div></a></li>';
+
+for(let i = 0; i < concertsJson.responseJSON.results.length;i++ ){
+    
+    let concert = concertsJson.responseJSON.results[i];
+    let li = '<li class="item"><a href="/tonleikar/" class="feature" itemscope itemtype="http://schema.org/MusicEvent"><div class="artwork"><div id="1_10185" class="zoom" style="background-image:url(';
+    li += concert.imageSource;
+    li += ');"></div></div><div class="caption"><div class="type music">T&#243;nleikar</div><div class="title"><h2 itemprop="name">';
+    li += concert.eventDateName;
+    li += '</h2></div><div class="venue" itemprop="location">';
+    li += concert.eventHallName;
+    li += '</div></div></a></li>';
+    
+    htmlCode += li;
 }
 
 mainFeatures.innerHTML = htmlCode;
 
+});
